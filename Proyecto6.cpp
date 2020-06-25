5d3f637bf0a66b7aabbe0d85b2c8e2364db87ee3
Salver DLCC <salver.osorio@outlook.com>
2020-06-25 18:10:06 -0500

    Se agrega fecha lanzamiento del catalogo de peliculas

--- Proyecto6.cpp
+++ Proyecto6.cpp
@@ -20,15 +20,23 @@ using namespace std;
 
 int main()
 {
-    char PeliName[20][100] = {"El Abogado del Diablo","Matrix","El observador","Dulce noviembre","El reto",
+    char PeliName[20][70] = {"El Abogado del Diablo","Matrix","El observador","Dulce noviembre","El reto",
                             "Matrix Reloaded","El Animatrix","Matrix Revoluciones","Constantine","La casa del lago",
-                            "Toy Story 4","John Wick 1: Otro día para matar","John Wick 2: Un nuevo día para matar","John Wick 3: Parabellum","Insidious",
-                            "Insidious","The Crazies","Stake land","Kill List","La cabaña en el bosque"};
-                            
-    char FechaLanzamiento[20][10] = {};
+                            "Toy Story 4","John Wick 1: Otro dia para matar","John Wick 2: Un nuevo dia para matar","John Wick 3: Parabellum","Insidious",
+                            "Insidious","The Crazies","Stake land","Kill List","La cabana en el bosque"};
+
+    char  FechaLanzamiento[20][5] = {"1997","1999","2000","2001","2003"
+                                    ,"2003","2003","2003","2005","2006"
+                                    ,"2019","2014","2017","2019","2010"
+                                    ,"2010","2019","2020","2011","2011"}; /*posiblemente se haga un parceo*/
     int opcion = 0;
 
-    do{
+    for(int i = 0; i < 20; i++)
+    {
+        cout<<i+1<<": "<<PeliName[i]<<"\t\t"<<FechaLanzamiento[i]<<endl;
+    }
+
+    /*do{
         cout<<"\tMenu principal"<<endl;
         cout<<"1. Mostrar peliculas desde las mas viejas a las mas nuevas"<<endl;
         cout<<"2. Mostrar peliculas desde las mas nuevas a las mas viejas"<<endl;
@@ -62,6 +70,7 @@ int main()
             break;
         }
     }while ( opcion != 4);
+    */
 
     return 0;
 }
