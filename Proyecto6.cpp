2bbae73f25c581ee408ebb7d4e90a6605492e6b8
Salver DLCC <salver.osorio@outlook.com>
2020-06-25 01:50:40 -0500

    add file

---
+++ Proyecto6.cpp
@@ -0,0 +1,25 @@
+/*Azul Web acaba de sacar una nueva plataforma de video, donde se van a publicar películas. Las películas tiene los siguientes atributos; Nombre de la película y Fecha de lanzamiento.
+
+Crea un programa en C++ que haga lo siguiente:
+
+1.- Permitir guardar 20 registros en una matriz; Nombre de película y año de lanzamiento.
+
+2.- Un menú que nos va a dar tres opciones:
+
+>> a) Mostrar películas desde las más viejas a las más nuevas.
+
+>> b) Mostrar películas desde las más nuevas a las más viejas.
+
+>> c) Mostrar películas de un año en concreto.
+
+3.- Opción de regresar al menú o salir del programa.*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
